07d496384808dfc2ef1515b99e7c3dd26aa87044
lin233 <790422967@qq.com>
2019-05-22 22:21:08 +0800

    add checkBox and dropBox test

--- project3/WebTest/src/main/java/test/OpenBrowsers.java
+++ project3/WebTest/src/main/java/test/OpenBrowsers.java
@@ -6,13 +6,19 @@ public class OpenBrowsers {
 
 
     public static void main(String args[]) throws Exception{
-        System.setProperty("webdriver.chrome.driver","/Users/myu/Downloads/chromedriver-2");
+        System.setProperty("webdriver.chrome.driver","G:\\大三下\\软件测试\\hw3\\新建文件夹\\chromedriver.exe");
 
         ChromeDriver driver = new ChromeDriver();
 
         LoginUtil lu=new LoginUtil();
-        //lu.addCookie(driver);
+//        lu.addCookie(driver);
         lu.getCookie(driver);
+
+        DropBoxTest dt = new DropBoxTest(driver);
+        dt.run();
+
+        CheckBoxTest ct = new CheckBoxTest(driver);
+        ct.run();
 
         ButtonTest bt=new ButtonTest(driver);
         bt.run();
